a55850d99bce0caac54ff7a46f73b78511e230bd
mariayanadev <yana.tech06@gmail.com>
2024-08-17 22:24:06 -0300

    Component of Heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { Button } from "./components/button"
+import { Button } from "./components/Button"
+import { Heading } from "./components/Heading" //Renomear: nomeComponente as novoNome
 
 function App() {
 
   return (
     <>
-    <h1 className= "text-dark-background"> Hello world! </h1>
+    <div className="bg-dark-background">
+    <div className="bg-green-200"> 
+    <Heading > Hello world! </Heading>
+    <Heading type = "h2" color="white"> Hello world of Heading 2 </Heading> 
+    </div>
     <Button> Primary Background </Button>
     <Button variant="secondary"> Secondary Background </Button>
     <Button isBold> Bold Text with Primary Background</Button>
     <Button isBold variant="secondary"> Bold Text with Secondary Background</Button>
+    </div>
     </>
   )
 }
