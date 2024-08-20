be6be76614f1709a1da7e3dc81138cf1137f5efa
mariayanadev <yana.tech06@gmail.com>
2024-08-20 20:29:22 -0300

    creating Section of Testimonials of Landing Page

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -7,13 +7,13 @@ import TestimonialPerson from "../../assets/testimonials/testimonial-1.png"
 export const Testimonials = () => {
     return (
         <section className="bg-white"> 
-            <div className="w-full max-w-[360px] mx-auto flex flex-col items-center justify-center gap-[80px]"> 
+            <div className="w-full max-w-[360px] md:max-w-[900px] mx-auto flex flex-col items-center justify-center gap-[80px] md:px-2 xl:px-0"> 
             <Heading type="h2" className="w-fit"> Testimonials </Heading> 
            
-           <div className="flex flex-col py-4 items-center justify-center gap-11">
+           <div className="flex flex-col py-4 items-center justify-center gap-11 md:gap-17">
             <img src={IbmLogo}/>
            
-            <p className="text-center text-sm text-secondary-text font-bold w-full max-w-[280px]"> Most calendars are designed for teams. SLate is deisgned for freelancers 
+            <p className="text-center text-sm text-secondary-text font-bold w-full max-w-[280px] md:max-w-none md:text-2xl md:font-normal"> Most calendars are designed for teams. Slate is deisgned for freelancers 
                 who want a simple way to plan their shedule.
             </p>
 
@@ -21,13 +21,13 @@ export const Testimonials = () => {
                 <img src={TestimonialPerson}/>
 
                 <div>
-                    <p> Organize acrossv</p>
-                    <p> UI designer </p>
+                    <p className="text-xs font-bold text-secondary-text"> Organize across</p>
+                    <p className="text-sm text-black"> UI designer </p>
                 </div>
             </div>
            </div>
 
-            <Button> More Testimonials </Button>
+            <Button className="w-full max-w-[200px]"> More Testimonials </Button>
         </div>
         </section>
     )
